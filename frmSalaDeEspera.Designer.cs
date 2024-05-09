20e5244b22aac6f69046e0a89976a9a2cc84ea96
Federico Holc <fholc@fi.uba.ar>
2024-05-09 10:42:40 -0300

    implementar vistas salas de espera

--- frmSalaDeEspera.Designer.cs
+++ frmSalaDeEspera.Designer.cs
@@ -31,11 +31,15 @@
             btnVolver = new Button();
             panel2 = new Panel();
             btnSalir = new Button();
+            lvwConTurno = new ListView();
+            lvwSinTurno = new ListView();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // btnVolver
             // 
-            btnVolver.Location = new Point(585, 401);
+            btnVolver.Location = new Point(565, 368);
             btnVolver.Name = "btnVolver";
             btnVolver.Size = new Size(75, 23);
             btnVolver.TabIndex = 42;
@@ -47,14 +51,14 @@
             // 
             panel2.BorderStyle = BorderStyle.FixedSingle;
             panel2.ForeColor = SystemColors.ActiveBorder;
-            panel2.Location = new Point(132, 388);
+            panel2.Location = new Point(112, 355);
             panel2.Name = "panel2";
             panel2.Size = new Size(528, 1);
             panel2.TabIndex = 41;
             // 
             // btnSalir
             // 
-            btnSalir.Location = new Point(504, 401);
+            btnSalir.Location = new Point(484, 368);
             btnSalir.Name = "btnSalir";
             btnSalir.Size = new Size(75, 23);
             btnSalir.TabIndex = 40;
@@ -62,11 +66,52 @@
             btnSalir.UseVisualStyleBackColor = true;
             btnSalir.Click += btnSalir_Click;
             // 
+            // lvwConTurno
+            // 
+            lvwConTurno.Location = new Point(22, 40);
+            lvwConTurno.Name = "lvwConTurno";
+            lvwConTurno.Size = new Size(628, 120);
+            lvwConTurno.TabIndex = 67;
+            lvwConTurno.UseCompatibleStateImageBehavior = false;
+            lvwConTurno.SelectedIndexChanged += lvwConTurno_SelectedIndexChanged;
+            // 
+            // lvwSinTurno
+            // 
+            lvwSinTurno.Location = new Point(22, 201);
+            lvwSinTurno.Name = "lvwSinTurno";
+            lvwSinTurno.Size = new Size(628, 120);
+            lvwSinTurno.TabIndex = 68;
+            lvwSinTurno.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(22, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(253, 20);
+            label1.TabIndex = 69;
+            label1.Text = "Sala de espera - Estudios con turno";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(22, 178);
+            label2.Name = "label2";
+            label2.Size = new Size(248, 20);
+            label2.TabIndex = 70;
+            label2.Text = "Sala de espera - Estudios sin turno";
+            // 
             // frmSalaDeEspera
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(672, 414);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lvwSinTurno);
+            Controls.Add(lvwConTurno);
             Controls.Add(btnVolver);
             Controls.Add(panel2);
             Controls.Add(btnSalir);
@@ -74,6 +119,7 @@
             Text = "Sala de Espera";
             Load += frmSalaDeEspera_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -81,5 +127,9 @@
         private Button btnVolver;
         private Panel panel2;
         private Button btnSalir;
+        private ListView lvwConTurno;
+        private ListView lvwSinTurno;
+        private Label label1;
+        private Label label2;
     }
 }
--- frmSalaDeEspera.cs
+++ frmSalaDeEspera.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Clinica.Datos;
+using Clinica.Entidades;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,6 +24,9 @@ namespace clinica
         private void frmSalaDeEspera_Load(object sender, EventArgs e)
         {
 
+            CargarEsperaListView(lvwConTurno, 1);
+            CargarEsperaListView(lvwSinTurno, 0);
+
         }
 
         private void btnVolver_Click(object sender, EventArgs e)
@@ -32,6 +38,265 @@ namespace clinica
         private void btnSalir_Click(object sender, EventArgs e)
         {
             Application.Exit();
+
+        }
+        /*private void CargarEspera(int idLugar)
+        {
+            lbxEnEspera.DataSource = null;
+            lbxEnEspera.Items.Clear();
+
+            MySqlConnection sqlCon = new MySqlConnection();
+            try
+            {
+
+                string query = "Select SalaDeEspera.FechaHoraAcreditacion, SalaDeEspera.Paciente_id, " +
+                    "Paciente.Apellido, Paciente.Nombre, SalaDeEspera.Estudio_id, Estudio.Descripcion, " +
+                    "SalaDeEspera.LugarDeAtencion_id, LugarDeAtencion.Descripcion, SalaDeEspera.Prioridad," +
+                    "SalaDeEspera.id " +
+                    "from saladeespera inner join Paciente on Paciente.id = SalaDeEspera.Paciente_id " +
+                    "inner join LugarDeAtencion on lugardeatencion.id = saladeespera.LugarDeAtencion_id " +
+                    "inner join Estudio on Estudio.id = saladeespera.Estudio_id " +
+                    $"where saladeespera.LugarDeAtencion_id = {idLugar} ";
+
+
+                query += " ORDER BY SalaDeEspera.Prioridad, SalaDeEspera.FechaHoraAcreditacion;";
+
+                sqlCon = Conexion.getInstancia().CrearConexion();
+
+                MySqlCommand comando = new(query, sqlCon)
+                {
+                    CommandType = CommandType.Text
+                };
+                sqlCon.Open();
+
+                MySqlDataReader reader;
+                reader = comando.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    List<KeyValuePair<int, string>> esperas = new();
+
+                    while (reader.Read())
+                    {
+
+                        string fecha = reader.GetDateTime(0).ToString();
+                        string pacienteApellido = reader.GetString(2);
+                        string pacienteNombre = reader.GetString(3);
+                        string estudioDescripcion = reader.GetString(5);
+                        string lugarDescripcion = reader.GetString(7);
+                        int prioridad = reader.GetInt32(8);
+                        int id = reader.GetInt32(9);
+
+
+                        string esperaDescripcion = $"{pacienteApellido}, {pacienteNombre} - " +
+                            $"{estudioDescripcion} - {fecha} - {prioridad}";
+
+                        KeyValuePair<int, string> espera = new(id, esperaDescripcion);
+                        esperas.Add(espera);
+
+                    }
+
+                    lbxEnEspera.DataSource = esperas;
+                    lbxEnEspera.DisplayMember = "Value";
+                    lbxEnEspera.SelectedIndex = 0;
+                    lbxEnEspera.Enabled = true;
+
+                }
+                else
+                {
+                    lbxEnEspera.DataSource = null;
+                    lbxEnEspera.Items.Clear();
+                    lbxEnEspera.Items.Add("No hay datos disponibles con los criterios seleccionados.");
+                    lbxEnEspera.Enabled = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }*/
+
+        private void CargarEsperaListView(ListView lvw, int requiereTurno)
+        {
+            lvw.Items.Clear();
+            lvw.Columns.Clear();
+
+            lvw.View = View.Details;
+
+            // Agregar columnas al ListView
+            lvw.Columns.Add("Paciente");
+            lvw.Columns.Add("Estudio");
+            lvw.Columns.Add("Lugar");
+            lvw.Columns.Add("Acreditación");
+            lvw.Columns.Add("Prioridad");
+
+
+            MySqlConnection sqlCon = new MySqlConnection();
+            try
+            {
+
+                string query = "Select SalaDeEspera.FechaHoraAcreditacion, SalaDeEspera.Paciente_id, " +
+                    "Paciente.Apellido, Paciente.Nombre, SalaDeEspera.Estudio_id, Estudio.Descripcion, " +
+                    "SalaDeEspera.LugarDeAtencion_id, LugarDeAtencion.Descripcion, SalaDeEspera.Prioridad," +
+                    "SalaDeEspera.id " +
+                    "from saladeespera inner join Paciente on Paciente.id = SalaDeEspera.Paciente_id " +
+                    "inner join LugarDeAtencion on lugardeatencion.id = saladeespera.LugarDeAtencion_id " +
+                    "inner join Estudio on Estudio.id = saladeespera.Estudio_id " +
+                    $"where Estudio.RequiereTurno = {requiereTurno} ";
+
+
+                query += " ORDER BY SalaDeEspera.Prioridad, SalaDeEspera.FechaHoraAcreditacion;";
+
+                sqlCon = Conexion.getInstancia().CrearConexion();
+
+                MySqlCommand comando = new(query, sqlCon)
+                {
+                    CommandType = CommandType.Text
+                };
+                sqlCon.Open();
+
+                MySqlDataReader reader;
+                reader = comando.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    List<KeyValuePair<int, string>> esperas = new();
+
+                    while (reader.Read())
+                    {
+
+                        string fecha = reader.GetDateTime(0).ToString();
+                        string pacienteApellido = reader.GetString(2);
+                        string pacienteNombre = reader.GetString(3);
+                        string estudioDescripcion = reader.GetString(5);
+                        string lugarDescripcion = reader.GetString(7);
+                        int prioridad = reader.GetInt32(8);
+                        int id = reader.GetInt32(9);
+
+                        string nombreyApellido = $"{pacienteApellido}, {pacienteNombre}";
+
+                        string[] row = { nombreyApellido, estudioDescripcion, lugarDescripcion, fecha, prioridad.ToString() };
+
+                        // Crear un ListViewItem con los valores de la fila actual
+                        ListViewItem item = new ListViewItem(row);
+
+                        // Establecer el valor de la propiedad Tag del ListViewItem con el id
+                        item.Tag = id;
+
+                        // Agregar el ListViewItem al ListView
+                        lvw.Items.Add(item);
+
+                    }
+
+                    lvw.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                    lvw.Columns[4].Width = 100;
+
+
+
+                }
+                else
+                {
+                    /* lbxEnEspera.DataSource = null;
+                     lbxEnEspera.Items.Clear();
+                     lbxEnEspera.Items.Add("No hay datos disponibles con los criterios seleccionados.");
+                     lbxEnEspera.Enabled = false;*/
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+       /* private void CargarLugares()
+        {
+            cbxLugar.DataSource = null;
+            cbxLugar.Items.Clear();
+            cbxLugar.Text = "";
+
+            MySqlConnection sqlCon = new MySqlConnection();
+            try
+            {
+                string query;
+                sqlCon = Conexion.getInstancia().CrearConexion();
+                query = "select LugarDeAtencion.id, LugarDeAtencion.Descripcion " +
+                    $"from LugarDeAtencion;";
+                MySqlCommand comando = new(query, sqlCon)
+                {
+                    CommandType = CommandType.Text
+                };
+                sqlCon.Open();
+
+                MySqlDataReader reader;
+                reader = comando.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    List<KeyValuePair<int, string>> lugares = new();
+
+                    while (reader.Read())
+                    {
+                        // Obtener el ID y el nombre de la cobertura
+                        int id = reader.GetInt32(0);
+                        string descripcion = reader.GetString(1);
+
+                        // Crear un objeto de KeyValuePair con el ID y el nombre de la cobertura
+                        KeyValuePair<int, string> lugar = new(id, descripcion);
+                        lugares.Add(lugar);
+
+                    }
+
+                    cbxLugar.DataSource = lugares;
+                    // Especificar qué propiedad del KeyValuePair se debe mostrar en el ComboBox 
+                    cbxLugar.DisplayMember = "Value";
+                    cbxLugar.SelectedIndex = -1;
+                }
+                else
+                {
+                    MessageBox.Show("No hay datos de Lugares");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }*/
+
+       /* private void cbxLugar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbxLugar.SelectedIndex != -1)
+            {
+                CargarEspera(((KeyValuePair<int, string>)cbxLugar.SelectedItem!).Key);
+                CargarEsperaListView(((KeyValuePair<int, string>)cbxLugar.SelectedItem!).Key);
+            }
+        }*/
+
+        private void lvwConTurno_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
