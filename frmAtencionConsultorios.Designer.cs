ab03d2f6c9a6c2bbe8e4580f0c04dca768f3bfc5
ML_DEV <martinlopez@bc.org.ar>
2024-06-05 14:33:21 -0300

    Cambios en Atención y Sala de espera del circuito A

---
+++ frmAtencionConsultorios.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace clinica
+{
+    partial class frmAtencionConsultorios
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnVolver = new Button();
+            btnSalir = new Button();
+            cbxLugar = new ComboBox();
+            label1 = new Label();
+            lbxEnEspera = new ListBox();
+            label2 = new Label();
+            rtxtHistoriaClinica = new RichTextBox();
+            label3 = new Label();
+            btnRegistrarAtencion = new Button();
+            btnPacienteAusente = new Button();
+            SuspendLayout();
+            // 
+            // btnVolver
+            // 
+            btnVolver.Location = new Point(149, 539);
+            btnVolver.Margin = new Padding(3, 4, 3, 4);
+            btnVolver.Name = "btnVolver";
+            btnVolver.Size = new Size(86, 31);
+            btnVolver.TabIndex = 0;
+            btnVolver.Text = "Volver";
+            btnVolver.UseVisualStyleBackColor = true;
+            btnVolver.Click += btnVolver_Click;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(14, 539);
+            btnSalir.Margin = new Padding(3, 4, 3, 4);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(86, 31);
+            btnSalir.TabIndex = 1;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // cbxLugar
+            // 
+            cbxLugar.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbxLugar.FormattingEnabled = true;
+            cbxLugar.Location = new Point(147, 29);
+            cbxLugar.Margin = new Padding(3, 4, 3, 4);
+            cbxLugar.Name = "cbxLugar";
+            cbxLugar.Size = new Size(394, 28);
+            cbxLugar.TabIndex = 2;
+            cbxLugar.SelectedIndexChanged += cbxLugar_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(186, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Seleccione un profesional :";
+            // 
+            // lbxEnEspera
+            // 
+            lbxEnEspera.FormattingEnabled = true;
+            lbxEnEspera.Location = new Point(14, 112);
+            lbxEnEspera.Margin = new Padding(3, 4, 3, 4);
+            lbxEnEspera.Name = "lbxEnEspera";
+            lbxEnEspera.Size = new Size(527, 144);
+            lbxEnEspera.TabIndex = 4;
+            lbxEnEspera.SelectedIndexChanged += lbxEnEspera_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 20);
+            label2.TabIndex = 5;
+            label2.Text = "En Espera :";
+            // 
+            // rtxtHistoriaClinica
+            // 
+            rtxtHistoriaClinica.Location = new Point(14, 301);
+            rtxtHistoriaClinica.Margin = new Padding(3, 4, 3, 4);
+            rtxtHistoriaClinica.Name = "rtxtHistoriaClinica";
+            rtxtHistoriaClinica.Size = new Size(527, 212);
+            rtxtHistoriaClinica.TabIndex = 6;
+            rtxtHistoriaClinica.Text = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(14, 277);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Historia Clínica :";
+            // 
+            // btnRegistrarAtencion
+            // 
+            btnRegistrarAtencion.Location = new Point(328, 539);
+            btnRegistrarAtencion.Margin = new Padding(3, 4, 3, 4);
+            btnRegistrarAtencion.Name = "btnRegistrarAtencion";
+            btnRegistrarAtencion.Size = new Size(214, 31);
+            btnRegistrarAtencion.TabIndex = 8;
+            btnRegistrarAtencion.Text = "Registrar Atención Paciente";
+            btnRegistrarAtencion.UseVisualStyleBackColor = true;
+            btnRegistrarAtencion.Click += btnRegistrarAtencion_Click;
+            // 
+            // btnPacienteAusente
+            // 
+            btnPacienteAusente.Location = new Point(328, 577);
+            btnPacienteAusente.Margin = new Padding(3, 4, 3, 4);
+            btnPacienteAusente.Name = "btnPacienteAusente";
+            btnPacienteAusente.Size = new Size(214, 31);
+            btnPacienteAusente.TabIndex = 9;
+            btnPacienteAusente.Text = "Registrar Paciente Ausente";
+            btnPacienteAusente.UseVisualStyleBackColor = true;
+            btnPacienteAusente.Click += btnPacienteAusente_Click;
+            // 
+            // frmAtencionConsultorios
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(555, 621);
+            Controls.Add(btnPacienteAusente);
+            Controls.Add(btnRegistrarAtencion);
+            Controls.Add(label3);
+            Controls.Add(rtxtHistoriaClinica);
+            Controls.Add(label2);
+            Controls.Add(lbxEnEspera);
+            Controls.Add(label1);
+            Controls.Add(cbxLugar);
+            Controls.Add(btnSalir);
+            Controls.Add(btnVolver);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "frmAtencionConsultorios";
+            Text = "Atención en Consultorios";
+            Load += frmAtencionConsultorios_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ComboBox cbxLugar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbxEnEspera;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtxtHistoriaClinica;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRegistrarAtencion;
+        private System.Windows.Forms.Button btnPacienteAusente;
+    }
+}
---
+++ frmAtencionConsultorios.cs
@@ -0,0 +1,277 @@
+﻿using Clinica.Datos;
+using Clinica.Entidades;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace clinica
+{
+    public partial class frmAtencionConsultorios : Form
+    {
+        private Form formOrigen;
+
+        public frmAtencionConsultorios(Form formOrigen)
+        {
+            InitializeComponent();
+            this.formOrigen = formOrigen;
+        }
+
+        private void frmAtencionConsultorios_Load(object sender, EventArgs e)
+        {
+            CargarLugares();
+            CargarEspera(-1);
+            rtxtHistoriaClinica.Text = "";
+            rtxtHistoriaClinica.Enabled = false;
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            formOrigen.Show();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CargarLugares()
+        {
+            cbxLugar.DataSource = null;
+            cbxLugar.Items.Clear();
+            cbxLugar.Text = "";
+
+            MySqlConnection sqlCon = new MySqlConnection();
+            try
+            {
+                string query;
+                sqlCon = Conexion.getInstancia().CrearConexion();
+                query = "SELECT profesional.Nombre, profesional.Apellido, profesional.Especialidad FROM profesional;";
+                MySqlCommand comando = new(query, sqlCon)
+                {
+                    CommandType = CommandType.Text
+                };
+                sqlCon.Open();
+
+                MySqlDataReader reader;
+                reader = comando.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    List<KeyValuePair<int, string>> lugares = new();
+                    while (reader.Read())
+                    {
+                        string nombre = reader.GetString(0);
+                        string apellido = reader.GetString(1);
+                        string especialidad = reader.GetString(2);
+
+                        string descripcion = $"{nombre} {apellido} - {especialidad}";
+                        KeyValuePair<int, string> lugar = new KeyValuePair<int, string>(0, descripcion); // Aquí ajusta el primer parámetro a tu necesidad
+                        lugares.Add(lugar);
+                    }
+                    cbxLugar.DataSource = lugares;
+                    cbxLugar.DisplayMember = "Value";
+                    cbxLugar.SelectedIndex = -1;
+                }
+                else
+                {
+                    MessageBox.Show("No hay datos de Profesionales");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+
+        private void cbxLugar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbxLugar.SelectedIndex != -1)
+            {
+                CargarEspera(((KeyValuePair<int, string>)cbxLugar.SelectedItem!).Key);
+            }
+        }
+
+        private void CargarEspera(int idLugar)
+        {
+            lbxEnEspera.DataSource = null;
+            lbxEnEspera.Items.Clear();
+
+            MySqlConnection sqlCon = new MySqlConnection();
+            try
+            {
+                string query = "SELECT SalaDeEspera.FechaHoraAcreditacion, SalaDeEspera.Paciente_id, " +
+                    "Paciente.Apellido, Paciente.Nombre, SalaDeEspera.Estudio_id, Estudio.Descripcion, " +
+                    "SalaDeEspera.LugarDeAtencion_id, LugarDeAtencion.Descripcion, SalaDeEspera.Prioridad, " +
+                    "SalaDeEspera.id " +
+                    "FROM SalaDeEspera " +
+                    "INNER JOIN Paciente ON Paciente.id = SalaDeEspera.Paciente_id " +
+                    "INNER JOIN LugarDeAtencion ON LugarDeAtencion.id = SalaDeEspera.LugarDeAtencion_id " +
+                    "INNER JOIN Estudio ON Estudio.id = SalaDeEspera.Estudio_id " +
+                    $"WHERE SalaDeEspera.LugarDeAtencion_id = {idLugar} " +
+                    "ORDER BY SalaDeEspera.Prioridad, SalaDeEspera.FechaHoraAcreditacion;";
+
+                sqlCon = Conexion.getInstancia().CrearConexion();
+                MySqlCommand comando = new(query, sqlCon)
+                {
+                    CommandType = CommandType.Text
+                };
+                sqlCon.Open();
+
+                MySqlDataReader reader;
+                reader = comando.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    List<KeyValuePair<int, string>> esperas = new();
+                    while (reader.Read())
+                    {
+                        string fecha = reader.GetDateTime(0).ToString();
+                        int pacienteId = reader.GetInt32(1);
+                        string pacienteApellido = reader.GetString(2);
+                        string pacienteNombre = reader.GetString(3);
+                        string estudioDescripcion = reader.GetString(5);
+                        string lugarDescripcion = reader.GetString(7);
+                        int prioridad = reader.GetInt32(8);
+                        int id = reader.GetInt32(9);
+
+                        string esperaDescripcion = $"{pacienteApellido}, {pacienteNombre} - {estudioDescripcion} - {fecha} - {prioridad}";
+                        KeyValuePair<int, string> espera = new(pacienteId, esperaDescripcion);
+                        esperas.Add(espera);
+                    }
+                    lbxEnEspera.DataSource = esperas;
+                    lbxEnEspera.DisplayMember = "Value";
+                    lbxEnEspera.SelectedIndex = 0;
+                    lbxEnEspera.Enabled = true;
+                }
+                else
+                {
+                    lbxEnEspera.DataSource = null;
+                    lbxEnEspera.Items.Clear();
+                    lbxEnEspera.Items.Add("No hay datos disponibles con los criterios seleccionados.");
+                    lbxEnEspera.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+
+        private void lbxEnEspera_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rtxtHistoriaClinica.Text = "";
+
+            if (lbxEnEspera.SelectedIndex != -1)
+            {
+                KeyValuePair<int, string> espera = (KeyValuePair<int, string>)lbxEnEspera.SelectedItem!;
+                int pacienteId = espera.Key;
+                string? historiaClinica = Clinica.Clinica.ObtenerHistoriaClinica(pacienteId);
+                if (historiaClinica != null)
+                {
+                    rtxtHistoriaClinica.Text = historiaClinica;
+                    rtxtHistoriaClinica.Enabled = true;
+                }
+                else
+                {
+                    rtxtHistoriaClinica.Text = "No hay historia clínica para este paciente.";
+                }
+            }
+        }
+
+        private void btnRegistrarAtencion_Click(object sender, EventArgs e)
+        {
+            int idPaciente = ((KeyValuePair<int, string>)lbxEnEspera.SelectedItem!).Key;
+            int idLugar = ((KeyValuePair<int, string>)cbxLugar.SelectedItem!).Key;
+
+            string historiaClinica = rtxtHistoriaClinica.Text + "\n> Paciente atendido - " + DateTime.Now.ToString() + " - " + cbxLugar.Text + ".";
+            int rtaHistoriaClinica = Clinica.Clinica.GuardarHistoriaClinica(idPaciente, historiaClinica);
+            if (rtaHistoriaClinica < 1)
+            {
+                MessageBox.Show("Error al guardar la historia clínica del paciente.");
+            }
+
+            int rtaQuitarSala = QuitarDeSala(idPaciente, idLugar);
+            if (rtaQuitarSala > 0)
+            {
+                MessageBox.Show("Se registró la atención del Paciente y se lo quitó de la lista de espera.");
+                CargarEspera(idLugar);
+            }
+            else
+            {
+                MessageBox.Show("Error al registrar la atención del paciente.");
+            }
+        }
+
+        private int QuitarDeSala(int idPaciente, int idLugar)
+        {
+            int salida = 0;
+            MySqlConnection sqlCon = new MySqlConnection();
+            try
+            {
+                sqlCon = Conexion.getInstancia().CrearConexion();
+                sqlCon.Open();
+                string query = $"DELETE FROM SalaDeEspera WHERE Paciente_id = {idPaciente} AND LugarDeAtencion_id = {idLugar};";
+                MySqlCommand comando = new MySqlCommand(query, sqlCon);
+                comando.Parameters.AddWithValue("@idPaciente", idPaciente);
+                comando.Parameters.AddWithValue("@idLugar", idLugar);
+                int rowsAffected = comando.ExecuteNonQuery();
+                salida = rowsAffected;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+            return salida;
+        }
+
+        private void btnPacienteAusente_Click(object sender, EventArgs e)
+        {
+            int idPaciente = ((KeyValuePair<int, string>)lbxEnEspera.SelectedItem!).Key;
+            int idLugar = ((KeyValuePair<int, string>)cbxLugar.SelectedItem!).Key;
+
+            string historiaClinica = rtxtHistoriaClinica.Text + "\n> Paciente ausente - " + DateTime.Now.ToString() + " - " + cbxLugar.Text + ".";
+            int rtaHistoriaClinica = Clinica.Clinica.GuardarHistoriaClinica(idPaciente, historiaClinica);
+            if (rtaHistoriaClinica < 1)
+            {
+                MessageBox.Show("Error al guardar la historia clínica del paciente.");
+            }
+
+            int rtaQuitarSala = QuitarDeSala(idPaciente, idLugar);
+            if (rtaQuitarSala > 0)
+            {
+                MessageBox.Show("Se registró la ausencia del Paciente y se lo quitó de la lista de espera.");
+                CargarEspera(idLugar);
+            }
+            else
+            {
+                MessageBox.Show("Error al registrar la ausencia del paciente.");
+            }
+        }
+    }
+}
+
